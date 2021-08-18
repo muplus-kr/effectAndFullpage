583f829b633fed904fceca77e3e0e920d6ce1ac2
parkjoonhwan <ikwladu2@muplus.co.kr>
2021-08-18 09:34:43 +0900

    js 수정

--- resource/js/common.js
+++ resource/js/common.js
@@ -4,9 +4,6 @@ $(document).ready(function () {
         navigation: true,
         responsiveWidth: 700,
         anchors: ["section1", "section2", "section3", "section4", "footer"],
-        onLeave: function (origin, destination, direction) {
-            // console.log("Leaving section" + origin.index);
-        },
     });
 });
 
